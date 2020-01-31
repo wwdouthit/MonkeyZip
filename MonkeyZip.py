cf531499171ecfeccc71ff645e4007255699fecd
MonkeyBrat70 <wade.douthit@gmail.com>
2020-01-31 11:08:27 -0500

    Added a timer.

--- MonkeyZip.py
+++ MonkeyZip.py
@@ -1,5 +1,6 @@
 import threading
 import zipfile
+from timeit import default_timer as timer
 
 class AsyncZip(threading.Thread):
     def __init__(self, infile, outfile):
@@ -19,8 +20,10 @@ zipFileName = input('Zipped filename:  ')
 #TODO add an asynchronous timer to calculate the time used in the zip process
 #TODO get/print the original size, compression size, and crc32 value
 background = AsyncZip(fileToZip, zipFileName)
+start = timer()
 background.start()
 print('The main program continues to run in the foreground.')
 
 background.join()
-print('Main program waited util background was done.')
+end = timer()
+print('It took {} seconds to zip the file.'.format(end - start))
