73dd86fb8609a1155a5002ccecca85c32c848371
MonkeyBrat70 <wade.douthit@gmail.com>
2020-01-31 09:55:25 -0500

    Built the basic async framework

---
+++ MonkeyZip.py
@@ -0,0 +1,26 @@
+import threading
+import zipfile
+
+class AsyncZip(threading.Thread):
+    def __init__(self, infile, outfile):
+        threading.Thread.__init__(self)
+        self.infile = infile
+        self.outfile = outfile
+
+    def run(self):
+        f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
+        f.write(self.infile)
+        f.close()
+        print('Finished background zip of :', self.infile)
+
+fileToZip = input('File you want to zip:  ')
+zipFileName = input('Zipped filename:  ')
+
+#TODO add an asynchronous timer to calculate the time used in the zip process
+#TODO get/print the original size, compression size, and crc32 value
+background = AsyncZip(fileToZip, zipFileName)
+background.start()
+print('The main program continues to run in the foreground.')
+
+background.join()
+print('Main program waited util background was done.')
--- README.md
+++
@@ -1,1 +0,0 @@
-# MonkeyZip
